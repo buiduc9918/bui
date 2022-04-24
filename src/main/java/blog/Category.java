146139862c289b77cd92c5ba1d6fcdcedc96d12c
buiduc9918 <97076379+buiduc9918@users.noreply.github.com>
2022-04-24 20:30:15 +0700

    first commit

---
+++ src/main/java/blog/Category.java
@@ -0,0 +1,46 @@
+package blog;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String des;
+	
+	private Date createdAt;
+
+	private Date updatedAt;
+	
+	@OneToMany(mappedBy = "cate")
+	private List<Post> listPost;
+
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+		this.updatedAt = new Date();
+	}
+	
+	@PreUpdate
+	void updatedAt() {
+		this.updatedAt = new Date();
+	}
+}
